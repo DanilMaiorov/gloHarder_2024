09a333792da3dfb0384fb49ed3d88444291c0d7c
damayorov93@gmail.com <damayorov93@gmail.com>
2024-02-17 13:50:30 +0500

    lesson_02

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('asd');
+// lesson_02
+let num = 266219;
+
+let nums = num.toString().split('');
+
+const multi = (result) => {
+  result = (result ** 3).toString().slice(0,2)
+  console.log(result);
+}
+
+/* 1 */
+const res1 = nums.reduce(function (acc, curr) {
+  return (acc * curr)
+})
+
+multi(res1);
+
+/* 2 */
+let res2 = 1;
+
+for (let i = 0; i < nums.length; i++) {
+  const el = nums[i];
+  res2 = res2 * +el
+}
+
+multi(res2);
